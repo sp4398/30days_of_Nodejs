4ec8f7d97889e0c05342f56734d45b5706065464
Saurav Pandey <sp4398@gmail.com>
2024-02-09 20:37:29 +0530

    node9

---
+++ day9.js
@@ -0,0 +1,19 @@
+/*
+Which of the following are examples of node modules?
+
+a. Express
+
+b. Body-parser
+
+c. Socket.io
+
+d. All of the above
+
+ans: d. All of the above
+
+Explanation: Express used for building web applications and APIs.
+             Body-parser is middleware for handling incoming request bodies in Express.js.
+            Socket.io used to communication between web clients and servers.
+
+            
+ */
